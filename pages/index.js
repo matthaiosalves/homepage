63fa8842fedfd294529aa7baa7d547f2cabace06
Matheus Alves <lucas24417@gmail.com>
2022-03-01 11:14:09 -0300

    ✨ Imported linkedin icon in next

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
-import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
+import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5'
 
 import thumbYouTube from '../public/images/works/youtube-thumb.jpg'
 
